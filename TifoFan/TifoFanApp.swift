445a729a4df0bf10ee55406908204dfb0cc64d3c
RazielHernandez <razo.puma.azteca@gmail.com>
2026-02-04 13:12:03 -0500

    first commit

---
+++ TifoFan/TifoFanApp.swift
@@ -0,0 +1,17 @@
+//
+//  TifoFanApp.swift
+//  TifoFan
+//
+//  Created by Puma Azteca on 2026-02-04.
+//
+
+import SwiftUI
+
+@main
+struct TifoFanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
